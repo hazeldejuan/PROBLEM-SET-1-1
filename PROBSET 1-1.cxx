6df29f0723a9ee2e191149b3ad73b4a421a9f34b
hazeldejuan <hazeldejuan217@gmail.com>
2025-05-05 21:45:54 +0800

    Add files via upload

---
+++ PROBSET 1-1.cxx
@@ -0,0 +1,11 @@
+ #include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    float a;
+    cout << "Enter your number" << endl;
+    cin >> a;
+    cout << a + 5;
+    return 0;
+}
